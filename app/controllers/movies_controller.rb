763a2d4b803f2b6f1f40ca51b7f33e9330098118
Hxw-w <74304447+Hxw-w@users.noreply.github.com>
2025-05-02 19:16:58 +0000

    Added create and delete links

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,16 @@
 class MoviesController < ApplicationController
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+  
   def create
     # Retrieve the user's input from params
     # Create a record in the movie table
@@ -7,6 +19,17 @@ class MoviesController < ApplicationController
 
     # Redirect the user back to the /movie URL
     
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
   end
   
   def index
