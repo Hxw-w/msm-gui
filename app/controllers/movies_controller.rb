8e649d8960c2461b22c995c3b88f0a47ec00b4ae
Hxw-w <74304447+Hxw-w@users.noreply.github.com>
2025-05-02 18:50:49 +0000

    initial commits

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,14 @@
 class MoviesController < ApplicationController
+  def create
+    # Retrieve the user's input from params
+    # Create a record in the movie table
+    # Populate each column with teh user input
+    # Save
+
+    # Redirect the user back to the /movie URL
+    
+  end
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
